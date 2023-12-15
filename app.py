4b9fa0104588e02778b7068fc187319cc1c83810
Danziel Cempron <104302220+Dnzldotexe@users.noreply.github.com>
2023-12-15 03:31:11 +0000

    Change icon from robot to ram

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title='B1TSPLZ', page_icon=':robot_space:')
+st.set_page_config(page_title='B1TSPLZ', page_icon=':ram:')
 
 st.title('Welcome to B1TSPLZ')
